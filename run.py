f38314af693a2ea513cefa2db2383e8ece71681c
Darran Henry <henrydarran@yahoo.com>
2024-10-23 07:39:06 +0000

    CALCULATING SURPLUS DATA

--- run.py
+++ run.py
@@ -66,6 +66,18 @@ def update_sales_worksheet(data):
     print("Sales worksheet updated successfully.\n")
 
 
+def calculate_surplus_data(sales_row):
+     """
+    Compare sales with stock and calculate the surplus for each item type.
+
+    The surplus is defined as the sales figure subtracted from the stock:
+    - Positive surplus indicates waste
+    - Negative surplus indicates extra made when stock was sold out.
+    """
+    print("Calculating surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    pprint(stock)
+
 def main():
     """
     Run all program functions
@@ -73,6 +85,11 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
 
 
 print("Welcome to Love Sandwiches Data Automation")
+main()
+
+
+
