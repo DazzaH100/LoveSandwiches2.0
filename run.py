878d0ac840a6bd5e0164a599cb2e39b8ea0b107e
Darran Henry <henrydarran@yahoo.com>
2024-10-23 07:26:00 +0000

    ADDING MAIN

--- run.py
+++ run.py
@@ -66,20 +66,6 @@ def update_sales_worksheet(data):
     print("Sales worksheet updated successfully.\n")
 
 
-def calculate_surplus_data(sales_row):
-    """
-    Compare sales with stock and calculate the surplus for each item type.
-
-    The surplus is defined as the sales figure subtracted from the stock:
-    - Positive surplus indicates waste
-    - Negative surplus indicates extra made when stock was sold out.
-    """
-    print("Calculating surplus data...\n")
-    stock = SHEET.worksheet("stock").get_all_values()
-    stock_row = stock[-1]
-    print(stock_row)
-
-
 def main():
     """
     Run all program functions
@@ -90,4 +76,3 @@ def main():
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
